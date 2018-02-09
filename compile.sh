2ccf2388a88d42614443cfbf7fe5eb6de3778b85
Josh Borrow <joshua.borrow@durham.ac.uk>
2018-02-09 17:24:58 +0000

    Added overview of the paper

--- compile.sh
+++ compile.sh
@@ -2,10 +2,10 @@ rm -rf compiled
 
 mkdir compiled
 
-pdflatex -output-directory="compiled" report.tex
+xelatex -output-directory="compiled" report.tex
 
 bibtex compiled/text
 
-pdflatex -output-directory="compiled" report.tex
-pdflatex -output-directory="compiled" report.tex
+xelatex -output-directory="compiled" report.tex
+xelatex -output-directory="compiled" report.tex
 
