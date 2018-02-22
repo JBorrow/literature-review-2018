ee4a54fd445d1e488b8471031cec03fe8dd9f8e7
Josh Borrow <joshua.borrow@durham.ac.uk>
2018-02-22 19:48:41 +0000

    Added Richard's changes and bibliography

--- compile.sh
+++ compile.sh
@@ -3,9 +3,5 @@ rm -rf compiled
 mkdir compiled
 
 xelatex -output-directory="compiled" report.tex
-
-bibtex compiled/report
-
-xelatex -output-directory="compiled" report.tex
 xelatex -output-directory="compiled" report.tex
 
